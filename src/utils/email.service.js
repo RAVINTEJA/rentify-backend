3de1dcfd934a25eacbfe40c52ad11dabfe017faa
RAVINTEJA <ravinteja.chilukamari@gmail.com>
2024-05-26 23:46:41 +0530

    feat: Update email service to use Mailgun for sending interest emails
    The code changes in the `email.service.js` file update the email service to use Mailgun for sending interest emails. The `nodemailer` dependency is replaced with `mailgun.js` and the `sendMail` function is replaced with the `mg.messages.create` method. This change improves the reliability and deliverability of the emails.
    
    This commit message follows the established convention of starting with a verb in the imperative form, followed by a concise description of the changes made.

--- src/utils/email.service.js
+++ src/utils/email.service.js
@@ -1,27 +1,44 @@
-import nodemailer from 'nodemailer';
+import formData from "form-data";
+import Mailgun from 'mailgun.js';
+import prisma from "../prisma.js"
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp.ethereal.email',
-    port: 587,
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD
-  }
-});
+const mailgun = new Mailgun(formData);
+const mg = mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY });
 
 export const sendInterestEmail = async (ownerEmail, userEmail, property) => {
-  const mailOptions = {
-    from: process.env.EMAIL_USERNAME,
-    to: "ravinspamjunk@gmail.com",
-    subject: 'New Interest in Your Property',
-    text: `A user has expressed interest in your property. User email: ${userEmail}. Property details: ${JSON.stringify(property)}`
-  };
 
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.error(error);
-    } else {
-      console.log('Email sent: ' + info.response);
+  const user = await prisma.user.findUnique({
+    where: { email: userEmail },
+    select: {
+      firstName: true,
+      lastName: true,
+      email: true,
+      phone: true
     }
   });
-};
+
+  const owner = await prisma.user.findUnique({
+    where: { email: ownerEmail },
+    select: {
+      firstName: true,
+      lastName: true,
+      email: true,
+      phone: true
+    }
+  });
+
+  mg.messages.create(process.env.MAILGUN_DOMAIN, {
+    from: process.env.MAILGUN_FROM_EMAIL,
+    to: ownerEmail,
+    subject: 'New Interest in Your Property',
+    text: `A user has expressed interest in your property. User email: ${userEmail}. Property details: ${JSON.stringify(property)}`
+  }).then(msg => console.log(msg)).catch(err => console.error(err));
+
+  mg.messages.create(process.env.MAILGUN_DOMAIN, {
+    from: process.env.MAILGUN_FROM_EMAIL,
+    to: userEmail,
+    subject: 'Interest Expressed',
+    text: `You have expressed interest in a property. Property details: ${JSON.stringify(property)}`,
+    html: `<p>You have expressed interest in a property. Owner details:</p> ${JSON.stringify(owner)}`
+  }).then(msg => console.log(msg)).catch(err => console.error(err));
+}
